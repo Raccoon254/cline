a07ba86a7077481ac20ac1bf29832416a13efadd
raccoon254 <tomsteve187@gmail.com>
2024-04-19 12:39:09 +0300

    fields on user skills and added mastery level

--- database/migrations/2024_04_19_093705_create_user_skills_table.php
+++ database/migrations/2024_04_19_093705_create_user_skills_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('user_skills', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('skill_id')->constrained()->cascadeOnDelete();
+            $table->integer('mastery_level');
             $table->timestamps();
         });
     }

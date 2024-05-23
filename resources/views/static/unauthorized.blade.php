96180b57c6900b37be5ede4f7783c905b951abb7
raccoon254 <tomsteve187@gmail.com>
2024-05-23 08:44:38 +0300

    Styled the unauthorized page

--- resources/views/static/unauthorized.blade.php
+++ resources/views/static/unauthorized.blade.php
@@ -3,15 +3,17 @@
 @include('static.parts.head')
 <body class="antialiased bg-cover bg-custom-black bg-center" style="background-image: url('/bg.svg');">
 @include('layouts.partials.navbar')
-<div class="min-h-screen bg-custom-black flex items-center justify-center">
+<div class="min-h-screen bg-custom-black flex flex-col items-center justify-center">
+    <!-- User slash icon -->
+    <div class="ring h-14 center ring-red-600 rounded-full w-14">
+        <i class="fas fa-user-slash text-3xl text-gray-300"></i>
+    </div>
     <div class="mb-4 text-sm text-gray-300">
         {{ __('You are not authorized to access this page.') }}
     </div>
     {{ $message ?? 'Please contact admin' }}
     <div class="mt-4 flex items-center justify-between">
-        <button>
-            <a href="{{ route('dashboard') }}">Go Back</a>
-        </button>
+        <a class="btn" href="{{ route('dashboard') }}">Go Back</a>
     </div>
 </div>
 </body>
